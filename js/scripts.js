fa54d850b40d898199bc7f02d6202982a57ba9c5
John Sweet and Kyle Wymer <john@>
2019-01-30 19:58:50 -0800

    add additional math functions.

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,30 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
 alert(result);
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = subtract(number1, number2);
+alert(result);
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = multiply(number1, number2);
+alert(result);
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = divide(number1, number2);
+alert(result);
