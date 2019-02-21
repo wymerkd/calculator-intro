166116735af7951c95c817ffac57f22b5a3b0dc4
Kyle Wymer <wymerkd@gmail.com>
2019-02-20 19:04:30 -0800

    add git branch to compile form

--- js/scripts.js
+++ js/scripts.js
@@ -15,32 +15,21 @@ var divide = function(number1, number2) {
 };
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#output").text(result);
-  });
-  $("form#subtract").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#subtract1").val());
-    var number2 = parseInt($("#subtract2").val());
-    var result = subtract(number1, number2);
-    $("#output").text(result);
-  });
-  $("form#multiply").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#multiply1").val());
-    var number2 = parseInt($("#multiply2").val());
-    var result = multiply(number1, number2);
-    $("#output").text(result);
-  });
-  $("form#divide").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#divide1").val());
-    var number2 = parseInt($("#divide2").val());
-    var result = divide(number1, number2);
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator ==="add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
   });
 });
